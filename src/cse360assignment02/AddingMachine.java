71add7277799cf9f7fdaed09b293d96cffdb3062
Kevin Weinhold <kweinhold22@gmail.com>
2020-10-04 15:28:25 -0700

    AddingMachine class, all functions incorporated

--- src/cse360assignment02/AddingMachine.java
+++ src/cse360assignment02/AddingMachine.java
@@ -4,13 +4,20 @@
  *
  * @author  Kevin Weinhold
  * @version 1.0
- * @since   2014-03-31
+ * @since   2020-10-4
  */
 
+
+
 package cse360assignment02;
+import java.util.ArrayList; // import the ArrayList class
+
+
 
 public class AddingMachine {
     private int total;
+
+    ArrayList<String> memory = new ArrayList<>(); // Create an ArrayList object
 
     public AddingMachine () {
         /**
@@ -18,14 +25,18 @@ public class AddingMachine {
          * written in the class and will be where we add, subtract, get total, and
          * print to String.
          */
+        memory.add("0 ");
         total = 0;  // not needed - included for clarity
+
     }
 
     public int getTotal () {
         /**
          * This method is used to get the total number calculated in the class
          */
-        return 0;
+
+
+        return total;
     }
 
     public void add (int value) {
@@ -33,6 +44,12 @@ public class AddingMachine {
          * This method is used to add two integers.
          * @param value This is the first paramter to add method
          */
+
+        memory.add("+ ");
+        memory.add(Integer.toString(value) + " ");
+        total = total + value;
+
+
     }
 
     public void subtract (int value) {
@@ -40,18 +57,29 @@ public class AddingMachine {
          * This method is used to subtract two integers.
          * @param value This is the first paramter to subtract method
          */
+
+        memory.add("- ");
+        memory.add(Integer.toString(value) + " ");
+        total = total - value;
+
     }
 
     public String toString () {
         /**
          * This method prints values to a string
          */
-        return "";
+        StringBuilder output = new StringBuilder();
+        for(int i = 0; i < memory.size(); i++) {
+            output.append(memory.get(i));
+        }
+        return output.toString();
     }
 
     public void clear() {
         /**
          * This method clears the interface for the user
          */
+        memory.clear();
+        memory.add("0 ");
     }
 }
